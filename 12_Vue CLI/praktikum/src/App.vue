a4908b6bb732c928382031860d0b21680cfc2dd0
Roland Brilianto <rolandbrilianto@gmail.com>
2022-03-18 13:09:50 +0700

    inisialisasi cli dan menyelesaikan tugas

---
+++ 12_Vue CLI/praktikum/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <ol>
+      <li v-for="items in todos" :key="items.id">{{ items }}</li>
+    </ol>
+    <input type="text" v-model="todo" />
+    <input type="submit" value="Tambahkan" @click="TambahToDo(todo)" /> <br />
+    <p v-if="todos.length >= 4">Hebat!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      todo: "",
+      todos: [],
+    };
+  },
+  methods: {
+    TambahToDo(todo) {
+      this.todos.push(todo);
+      this.todo = "";
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
